2e812de56623b55e6acbc719a01fa73a7dc70d6b
erikdbrown <erikdbrown@gmail.com>
2015-11-18 20:59:36 -0800

    passed queue shared behavior: "reports a size of zero for a new queue"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,18 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var results = {};
+  results.size = queueMethods.size;
+  //results.queueSize = 0;
+  return results;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  pop : function() {}, 
+  push: function() {},
+  size: function() {
+    return 0;
+  }
+};
 
 
