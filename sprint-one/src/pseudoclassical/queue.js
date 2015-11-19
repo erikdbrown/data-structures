f389643e7c4f429c838491d16d23334c082e37f7
erikdbrown <erikdbrown@gmail.com>
2015-11-19 10:48:24 -0800

    passes "reports a size of zero for a new queue"

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.counter = 0;
+  this.storage = {};
 };
+
+Queue.prototype.size = function() {
+  return this.queueSize < 0 ? 0 : this.queueSize;
+}
+
 
 
