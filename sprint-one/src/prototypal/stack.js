2b052dd61370848f2e39cfc9657f3d484d34f9fc
erikdbrown <erikdbrown@gmail.com>
2015-11-18 21:43:39 -0800

    passes "reports a size of zero for a new stack," "does not error when removing from an empty stack," "reports a size of 0 after removing more items than were added" tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var results = Object.create(stackMethods);
+  results.stackSize = 0;
+  results.storage = {};
+
+  return results;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {};
+stackMethods.pop = function() {};
+stackMethods.size = function() {
+  return 0;
+};
 
 
