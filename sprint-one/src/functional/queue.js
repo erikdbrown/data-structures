97952fc5bbb4a41facdedbde9987c0c938fb73b7
erikdbrown <erikdbrown@gmail.com>
2015-11-18 16:41:39 -0800

    passess all tests without moving values within the 'storage' object

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,22 +2,41 @@ var Queue = function() {
   var someInstance = {};
   var stackSize = 0;
   var counter = 0;
-
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[stackSize] = value;
+    storage[counter] = value;
+    //{1:b, 2: c}
     stackSize++;
+    //st = 2;
+    counter++;
+    //v = 3:
   };
 
   someInstance.dequeue = function() {
+    var arr = [];
+    for (var key in storage) {
+        arr.push(key);
+        //arr = [0,1]
+        //arr = [1,2]
+    }
+    var value = storage[arr[0]]
+    //value = a;
+    //val = b:
     stackSize--;
-    var value = storage[0];
-    counter++;
+    //st = 1;
+    //st = 1;
+    delete storage[arr[0]];
+    //delete a IN storate
+    //delete b IN storage
     return value;
+    //return a
+    // c=2, st=1, {1:b}
+    //returb b
+    // c =3, st = 1, {2:c}
     
   };
 
