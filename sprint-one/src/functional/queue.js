6b437924373ae4f60d2e55e803311652b23d2fca
erikdbrown <erikdbrown@gmail.com>
2015-11-19 10:10:34 -0800

    dequeue operated without traversing array. Uses counter - queueSize

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,6 +1,6 @@
 var Queue = function() {
   var someInstance = {};
-  var stackSize = 0;
+  var queueSize = 0;
   var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
@@ -10,38 +10,24 @@ var Queue = function() {
   someInstance.enqueue = function(value) {
     storage[counter] = value;
     //{1:b, 2: c}
-    stackSize++;
+    queueSize++;
     //st = 2;
     counter++;
     //v = 3:
   };
 
   someInstance.dequeue = function() {
-    var arr = [];
-    for (var key in storage) {
-        arr.push(key);
-        //arr = [0,1]
-        //arr = [1,2]
-    }
-    var value = storage[arr[0]]
-    //value = a;
-    //val = b:
-    stackSize--;
-    //st = 1;
-    //st = 1;
-    delete storage[arr[0]];
-    //delete a IN storate
-    //delete b IN storage
-    return value;
-    //return a
-    // c=2, st=1, {1:b}
-    //returb b
-    // c =3, st = 1, {2:c}
-    
+   var val = storage[counter - queueSize];
+   queueSize--;
+   // delete storage[counter - queueSize];
+   if (queueSize < 0) {
+    queueSize = 0;
+   }
+   return val;
   };
 
   someInstance.size = function() {
-    return stackSize < 0 ? 0 : stackSize;
+    return queueSize < 0 ? 0 : queueSize;
   };
 
   return someInstance;
