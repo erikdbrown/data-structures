2e7ad31d26a9b6aa47034204f7b5f23b892f35c2
erikdbrown <erikdbrown@gmail.com>
2015-11-18 22:01:57 -0800

    Passes: "reports a size of zero for a new queue," "reports a size of 2 after adding two items," "does not error when removing from an empty queue," "reports a size of 1 after adding two items and removing one," "reports a size of 0 after removing more items than were added"

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
+  var results = Object.create(queueMethods);
+  results.queueSize = 0;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return results;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.queueSize > 0 ? this.queueSize : 0;
+};
+queueMethods.enqueue = function() {
+  this.queueSize++;
+};
+queueMethods.dequeue = function() {
+  this.queueSize--;
+};
 
 
