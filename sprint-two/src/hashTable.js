3787abb154bc6b2f80ee94679df63cf254c87f7f
erikdbrown <erikdbrown@gmail.com>
2015-11-20 12:19:47 -0800

    Passed all test!!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,38 +2,51 @@
 
 var HashTable = function() {
   this._limit = 8;
-  // var obj = Object.create(linkedList);
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // var value = this._storage.get(k)
-  // if (value !== undefined) 
-    // this._storage.each(function(item, i , collection) {
-      // if (i === index) {
-        // if (collection[i][bucket] !== undefined) {
-          // collection[i][bucket] = [];
-          // collection[i][bucket][value] = collection[i]
-        // } else {
-        // collection[i][bucket][k] === v;
-      // }
-    // else 
-      // })
-  this._storage.set(index, v);
+    var bucket = this._storage.get(index);
+    if (typeof bucket === "undefined") {
+      bucket = [];
+    }
+    var keyExists = false; 
+    _.each(bucket, function(tuple) {
+      if (tuple[0] === k) {
+        tuple[1] = v;
+        keyExists = true;
+      }
+    })
+    if (!keyExists) {
+     bucket.push([k,v]); 
+    }
+    this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  if (typeof bucket === 'undefined') {
+    return;
+  }
+
+  var returnValue;
+  _.each(bucket, function(tuple) {
+    if (tuple[0] === k) {
+      returnValue = tuple[1];
+    }
+  });
+
+  return returnValue;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var valToRemove = this._storage.get(index);
-  this._storage.each(function(item,i, collection) {
-    if (item === valToRemove) {
-      delete collection[i];
+  var bucket = this._storage.get(index); 
+  _.each(bucket, function(tuple, i) {
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
     }
   })
 };
@@ -43,5 +56,4 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
