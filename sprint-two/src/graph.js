21da9ad6d91a6e125e7994038188f267ee54046c
erikdbrown <erikdbrown@gmail.com>
2015-11-19 20:27:32 -0800

    graph passes all tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -50,6 +50,9 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.points, function(node) {
+    cb(node);
+  })
 };
 
 /*
